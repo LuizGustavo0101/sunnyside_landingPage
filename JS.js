be4112ba038a6ec65fdd5565918ac21160cab778
LuizGustavo0101 <111617474+LuizGustavo0101@users.noreply.github.com>
2023-08-09 14:40:38 -0300

    Add files via upload

--- JS.js
+++ JS.js
@@ -3,9 +3,14 @@ function responsividade(){
 
     if(larguraTela > 425){
         document.querySelector("#imagemHeader").src = "./images/desktop/image-header.jpg";
+        document.querySelector("#cerejaFoto").src = "./images/desktop/image-graphic-design.jpg";
+        document.querySelector("#laranjaFoto").src = "./images/desktop/image-photography.jpg";
     } else{
         document.querySelector("#imagemHeader").src = "./images/mobile/image-header.jpg";
+        document.querySelector("#cerejaFoto").src = "./images/mobile/image-graphic-design.jpg";
+        document.querySelector("#laranjajaFoto").src = "./images/mobile/image-photography.jpg";
     }
 }
 
 window.onresize = responsividade;
+window.onload = responsividade;
